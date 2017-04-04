a77a18c32934c5c3f86be16c3f42aa35f17c6c0b
cibernox <miguel.camba@gmail.com>
2017-04-04 09:36:23 +0100

    Remove phantomjs

--- testem.js
+++ testem.js
@@ -3,11 +3,11 @@ module.exports = {
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
   "launch_in_ci": [
-    "PhantomJS",
+    // "PhantomJS",
     "Chrome"
   ],
   "launch_in_dev": [
-    "PhantomJS",
+    // "PhantomJS",
     "Chrome"
   ],
   "browser_args": {
